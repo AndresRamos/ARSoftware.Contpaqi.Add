a3cc91e810d504038134538be5c07f2f44430a70
Andres Ramos <andres@arsoft.net>
2024-02-21 15:30:31 -0600

    Mapea base de datos Document Content

--- src/ARSoftware.Contpaqi.Add.Sql.Models/DocumentContent/DocumentContent.cs
+++ src/ARSoftware.Contpaqi.Add.Sql.Models/DocumentContent/DocumentContent.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ARSoftware.Contpaqi.Add.Sql.Models.DocumentContent
+namespace ARSoftware.Contpaqi.Add.Sql.Models.DocumentContent;
+
+public partial class DocumentContent
 {
-    public partial class DocumentContent
-    {
-        public Guid GuidDocument { get; set; }
-        public string? DocumentType { get; set; }
-        public string? FileName { get; set; }
-        public string? Content { get; set; }
-        public string? SubDirectory { get; set; }
-        public DateTime? DocumentDate { get; set; }
-        public DateTime? CreationDate { get; set; }
-    }
+    public Guid GuidDocument { get; set; }
+
+    public string? DocumentType { get; set; }
+
+    public string? FileName { get; set; }
+
+    public string? Content { get; set; }
+
+    public string? SubDirectory { get; set; }
+
+    public DateTime? DocumentDate { get; set; }
+
+    public DateTime? CreationDate { get; set; }
 }
