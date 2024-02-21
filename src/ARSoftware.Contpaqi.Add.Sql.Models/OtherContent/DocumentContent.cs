c7608e9da414d7579585aa50e92e3e83a888aa2d
Andres Ramos <andres@arsoft.net>
2024-02-21 15:31:47 -0600

    Mapea base de datos Other Content

--- src/ARSoftware.Contpaqi.Add.Sql.Models/OtherContent/DocumentContent.cs
+++ src/ARSoftware.Contpaqi.Add.Sql.Models/OtherContent/DocumentContent.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ARSoftware.Contpaqi.Add.Sql.Models.OtherContent
+namespace ARSoftware.Contpaqi.Add.Sql.Models.OtherContent;
+
+public partial class DocumentContent
 {
-    public partial class DocumentContent
-    {
-        public Guid GuidDocument { get; set; }
-        public string? DocumentType { get; set; }
-        public string? FileName { get; set; }
-        public string? Content { get; set; }
-        public string? SubDirectory { get; set; }
-        public DateTime? DocumentDate { get; set; }
-        public DateTime? CreationDate { get; set; }
-    }
+    public Guid GuidDocument { get; set; }
+
+    public string? DocumentType { get; set; }
+
+    public string? FileName { get; set; }
+
+    public string? Content { get; set; }
+
+    public string? SubDirectory { get; set; }
+
+    public DateTime? DocumentDate { get; set; }
+
+    public DateTime? CreationDate { get; set; }
 }
--- src/ARSoftware.Contpaqi.Add.Sql/Contexts/ContpaqiAddOtherContentDbContext.cs
+++ src/ARSoftware.Contpaqi.Add.Sql/Contexts/ContpaqiAddOtherContentDbContext.cs
@@ -1,81 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 using ARSoftware.Contpaqi.Add.Sql.Models.OtherContent;
+using Microsoft.EntityFrameworkCore;
 
-namespace ARSoftware.Contpaqi.Add.Sql.Contexts
+namespace ARSoftware.Contpaqi.Add.Sql.Contexts;
+
+public partial class ContpaqiAddOtherContentDbContext : DbContext
 {
-    public partial class ContpaqiAddOtherContentDbContext : DbContext
+    public ContpaqiAddOtherContentDbContext(DbContextOptions<ContpaqiAddOtherContentDbContext> options)
+        : base(options)
     {
-        public ContpaqiAddOtherContentDbContext()
-        {
-        }
-
-        public ContpaqiAddOtherContentDbContext(DbContextOptions<ContpaqiAddOtherContentDbContext> options)
-            : base(options)
-        {
-        }
-
-        public virtual DbSet<DocumentContent> DocumentContent { get; set; } = null!;
-        public virtual DbSet<MyCompanyProgObj> MyCompanyProgObj { get; set; } = null!;
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.UseCollation("Modern_Spanish_CI_AS");
-
-            modelBuilder.Entity<DocumentContent>(entity =>
-            {
-                entity.HasKey(e => e.GuidDocument);
-
-                entity.HasIndex(e => e.GuidDocument, "IXFK_DocumentContent");
-
-                entity.Property(e => e.GuidDocument).ValueGeneratedNever();
-
-                entity.Property(e => e.Content).HasColumnType("text");
-
-                entity.Property(e => e.CreationDate).HasColumnType("datetime");
-
-                entity.Property(e => e.DocumentDate).HasColumnType("datetime");
-
-                entity.Property(e => e.DocumentType)
-                    .HasMaxLength(50)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.FileName)
-                    .HasMaxLength(255)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SubDirectory)
-                    .HasMaxLength(2)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<MyCompanyProgObj>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.HasIndex(e => e.NameObj, "IC_MyCompanyProgObj")
-                    .IsClustered();
-
-                entity.HasIndex(e => new { e.NameObj, e.Ver }, "INC_MyCompanyProgObj");
-
-                entity.Property(e => e.NameObj)
-                    .HasMaxLength(150)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Ver)
-                    .HasMaxLength(25)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.XType)
-                    .HasMaxLength(2)
-                    .IsUnicode(false);
-            });
-
-            OnModelCreatingPartial(modelBuilder);
-        }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
+
+    public virtual DbSet<DocumentContent> DocumentContent { get; set; }
+
+    public virtual DbSet<MyCompanyProgObj> MyCompanyProgObj { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("Modern_Spanish_CI_AS");
+
+        modelBuilder.Entity<DocumentContent>(entity =>
+        {
+            entity.HasKey(e => e.GuidDocument);
+
+            entity.HasIndex(e => e.GuidDocument, "IXFK_DocumentContent");
+
+            entity.Property(e => e.GuidDocument).ValueGeneratedNever();
+            entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            entity.Property(e => e.DocumentDate).HasColumnType("datetime");
+            entity.Property(e => e.DocumentType)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FileName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.SubDirectory)
+                .HasMaxLength(2)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<MyCompanyProgObj>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.HasIndex(e => e.NameObj, "IC_MyCompanyProgObj").IsClustered();
+
+            entity.HasIndex(e => new { e.NameObj, e.Ver }, "INC_MyCompanyProgObj");
+
+            entity.Property(e => e.NameObj)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Ver)
+                .HasMaxLength(25)
+                .IsUnicode(false);
+            entity.Property(e => e.XType)
+                .HasMaxLength(2)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
